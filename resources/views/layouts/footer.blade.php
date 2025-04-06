7097788f37870cbd3cbe777a78296159fa8c5a78
juan.diaz <rulos26@gmail.com>
2025-04-05 19:52:59 -0500

    cambiar el nombre de la marca en el pie de página y la barra lateral a ModuStackPetLTE

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-inline">
-        NovaventaCaro
+        ModuStackPetLTE
     </div>
     <strong>Copyright &copy; {{ date('Y') }} <a href="#">RulosSoluciones.com </a>.</strong> Todos los derechos reservados.
 </footer>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url('/') }}" class="brand-link">
-        <span class="brand-text font-weight-light">NovavenraCaroLTE</span>
+        <span class="brand-text font-weight-light">ModuStackPetLTE</span>
     </a>
     <div class="sidebar">
         <!-- Permisos por rol -->
