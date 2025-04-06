540dd7cdce8e941177b79d06718a441b8e8bead6
juan.diaz <rulos26@gmail.com>
2025-04-05 20:11:00 -0500

    reorganizar el menú de navegación para mejorar la accesibilidad y la presentación del botón de menú lateral

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,15 +1,6 @@
 <nav class="main-header navbar navbar-expand navbar-light bg-white shadow-sm">
-    <!-- Botón para mostrar/ocultar el menú lateral -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="offcanvas" href="#" role="button">
-                <i class="fas fa-bars"></i>
-            </a>
-        </li>
-    </ul>
-
     <!-- Información del usuario -->
-    <ul class="navbar-nav ms-auto">
+    <ul class="navbar-nav">
         <li class="nav-item dropdown">
             <!-- Botón desplegable con imagen y nombre del usuario -->
             <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -20,7 +11,7 @@
             </a>
 
             <!-- Menú desplegable -->
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+            <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="userDropdown">
                 <!-- Información del usuario -->
                 <li class="dropdown-item text-center">
                     <strong>{{ auth()->user()->email }}</strong>
@@ -46,6 +37,15 @@
                     </a>
                 </li>
             </ul>
+        </li>
+    </ul>
+
+    <!-- Botón para mostrar/ocultar el menú lateral -->
+    <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="offcanvas" href="#" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
         </li>
     </ul>
 </nav>
