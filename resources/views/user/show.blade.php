35437c0afa2d40491024a4e4433ba9b084d40d9d
juan.diaz <rulos26@gmail.com>
2025-04-08 20:02:21 -0500

    Agregar múltiples estilos de visualización para el perfil de usuario

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,12 +7,14 @@
 @section('content')
     <section class="content container-fluid">
         <div class="row">
+            <!-- Estilo 1: Profile Widget -->
             <div class="col-md-4">
-                <!-- Tarjeta de Perfil -->
-                <div class="card card-primary card-outline">
-                    <div class="card-body box-profile">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ __('Perfil de Usuario (Widget)') }}</h3>
+                    </div>
+                    <div class="card-body">
                         <div class="text-center">
-                            <!-- Foto de Perfil -->
                             @if($user->avatar && file_exists(public_path($user->avatar)))
                                 <img class="profile-user-img img-fluid img-circle"
                                      src="{{ asset('public/' . $user->avatar) }}"
@@ -23,10 +25,8 @@
                                      alt="Foto de Perfil">
                             @endif
                         </div>
-
                         <h3 class="profile-username text-center">{{ $user->name }}</h3>
                         <p class="text-muted text-center">{{ __('Usuario Activo') }}</p>
-
                         <ul class="list-group list-group-unbordered mb-3">
                             <li class="list-group-item">
                                 <b>{{ __('Correo Electrónico') }}</b> <a class="float-right">{{ $user->email }}</a>
@@ -38,52 +38,76 @@
                                 <b>{{ __('WhatsApp') }}</b> <a class="float-right">{{ $user->whatsapp }}</a>
                             </li>
                         </ul>
+                        <a href="{{ route('users.index') }}" class="btn btn-primary btn-block"><b>{{ __('Volver') }}</b></a>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-8">
-                <!-- Información Detallada -->
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">{{ __('Información del Usuario') }}</h5>
+            <!-- Estilo 2: User Card -->
+            <div class="col-md-4">
+                <div class="card card-widget widget-user">
+                    <div class="widget-user-header bg-primary">
+                        <h3 class="widget-user-username">{{ $user->name }}</h3>
+                        <h5 class="widget-user-desc">{{ __('Usuario Activo') }}</h5>
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <strong>{{ __('Tipo de Documento:') }}</strong>
-                                    {{ $user->tipo_documento }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Cédula:') }}</strong>
-                                    {{ $user->cedula }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Fecha de Nacimiento:') }}</strong>
-                                    {{ $user->fecha_nacimiento }}
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <strong>{{ __('Activo:') }}</strong>
-                                    {{ $user->activo ? __('Sí') : __('No') }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Creado el:') }}</strong>
-                                    {{ $user->created_at->format('d/m/Y') }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Última Actualización:') }}</strong>
-                                    {{ $user->updated_at->format('d/m/Y') }}
-                                </div>
-                            </div>
+                    <div class="widget-user-image">
+                        @if($user->avatar && file_exists(public_path($user->avatar)))
+                            <img class="img-circle elevation-2" src="{{ asset('public/' . $user->avatar) }}" alt="Foto de Perfil">
+                        @else
+                            <img class="img-circle elevation-2" src="{{ asset('default-avatar.png') }}" alt="Foto de Perfil">
+                        @endif
+                    </div>
+                    <div class="card-footer">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <span class="nav-link">
+                                    {{ __('Correo Electrónico') }} <span class="float-right">{{ $user->email }}</span>
+                                </span>
+                            </li>
+                            <li class="nav-item">
+                                <span class="nav-link">
+                                    {{ __('Teléfono') }} <span class="float-right">{{ $user->telefono }}</span>
+                                </span>
+                            </li>
+                            <li class="nav-item">
+                                <span class="nav-link">
+                                    {{ __('WhatsApp') }} <span class="float-right">{{ $user->whatsapp }}</span>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Estilo 3: Profile Overlay -->
+            <div class="col-md-4">
+                <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                        <div class="text-center">
+                            @if($user->avatar && file_exists(public_path($user->avatar)))
+                                <img class="profile-user-img img-fluid img-circle"
+                                     src="{{ asset('public/' . $user->avatar) }}"
+                                     alt="Foto de Perfil">
+                            @else
+                                <img class="profile-user-img img-fluid img-circle"
+                                     src="{{ asset('default-avatar.png') }}" 
+                                     alt="Foto de Perfil">
+                            @endif
                         </div>
-                        <div class="row mt-3">
-                            <div class="col-md-12">
-                                <a href="{{ route('users.index') }}" class="btn btn-secondary">{{ __('Volver') }}</a>
-                            </div>
-                        </div>
+                        <h3 class="profile-username text-center">{{ $user->name }}</h3>
+                        <p class="text-muted text-center">{{ __('Usuario Activo') }}</p>
+                        <ul class="list-group list-group-unbordered mb-3">
+                            <li class="list-group-item">
+                                <b>{{ __('Correo Electrónico') }}</b> <a class="float-right">{{ $user->email }}</a>
+                            </li>
+                            <li class="list-group-item">
+                                <b>{{ __('Teléfono') }}</b> <a class="float-right">{{ $user->telefono }}</a>
+                            </li>
+                            <li class="list-group-item">
+                                <b>{{ __('WhatsApp') }}</b> <a class="float-right">{{ $user->whatsapp }}</a>
+                            </li>
+                        </ul>
+                        <a href="{{ route('users.index') }}" class="btn btn-primary btn-block"><b>{{ __('Volver') }}</b></a>
                     </div>
                 </div>
             </div>
