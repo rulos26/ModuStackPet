52a8b8d27c1e0084f08f2a754cec2d043cde833b
juan.diaz <rulos26@gmail.com>
2025-04-09 10:10:17 -0500

    Actualizar vista de roles de usuario: mejorar diseño y estructura de la tabla de asignación de roles

--- resources/views/user/roles.blade.php
+++ resources/views/user/roles.blade.php
@@ -1,44 +1,70 @@
 @extends('layouts.app')
 
-@section('title', 'Asignar Roles')
+@section('template_title')
+    Asignar Roles
+@endsection
 
 @section('content')
-<div class="container mt-4">
-    <h3>Asignar Roles a Usuarios</h3>
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-    <table class="table table-bordered table-hover mt-3">
-        <thead>
-            <tr>
-                <th>Usuario</th>
-                <th>Email</th>
-                <th>Roles actuales</th>
-                <th>Acceso rápido</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($usuarios as $usuario)
-                <tr>
-                    <td>{{ $usuario->name }}</td>
-                    <td>{{ $usuario->email }}</td>
-                    <td>{{ $usuario->roles->pluck('name')->join(', ') }}</td>
-                    <td>
-                        <form action="{{ route('usuarios.roles.asignar', $usuario) }}" method="POST" class="d-inline">
-                            @csrf
-                            <input type="hidden" name="rol" value="Cliente">
-                            <button type="submit" class="btn btn-outline-primary btn-sm">Cliente</button>
-                        </form>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <span id="card_title">
+                                {{ __('Asignar Roles a Usuarios') }}
+                            </span>
+                        </div>
+                    </div>
 
-                        <form action="{{ route('usuarios.roles.asignar', $usuario) }}" method="POST" class="d-inline ms-2">
-                            @csrf
-                            <input type="hidden" name="rol" value="Paseador">
-                            <button type="submit" class="btn btn-outline-success btn-sm">Paseador</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+                    @if(session('success'))
+                        <div class="alert alert-success m-4">
+                            <p>{{ session('success') }}</p>
+                        </div>
+                    @endif
+
+                    <div class="card-body bg-white">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Usuario</th>
+                                        <th>Email</th>
+                                        <th>Roles actuales</th>
+                                        <th>Acceso rápido</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($usuarios as $index => $usuario)
+                                        <tr>
+                                            <td>{{ $index + 1 }}</td>
+                                            <td>{{ $usuario->name }}</td>
+                                            <td>{{ $usuario->email }}</td>
+                                            <td>{{ $usuario->roles->pluck('name')->join(', ') }}</td>
+                                            <td>
+                                                <form action="{{ route('usuarios.roles.asignar', $usuario) }}" method="POST" class="d-inline">
+                                                    @csrf
+                                                    <input type="hidden" name="rol" value="Cliente">
+                                                    <button type="submit" class="btn btn-outline-primary btn-sm">Cliente</button>
+                                                </form>
+
+                                                <form action="{{ route('usuarios.roles.asignar', $usuario) }}" method="POST" class="d-inline ms-2">
+                                                    @csrf
+                                                    <input type="hidden" name="rol" value="Paseador">
+                                                    <button type="submit" class="btn btn-outline-success btn-sm">Paseador</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {{-- Paginación (si es necesaria) --}}
+                {!! $usuarios->links() !!}
+            </div>
+        </div>
+    </div>
 @endsection
