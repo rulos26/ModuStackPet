2d4fc35aabc0337f3cebcc9bf4f09c27e6265213
juan.diaz <rulos26@gmail.com>
2025-04-10 13:52:51 -0500

    Actualizar la vista de detalles de la mascota: se mejora la presentación de la información utilizando un diseño de tarjeta más claro, se eliminan campos innecesarios y se reorganizan los datos para una mejor legibilidad.

--- resources/views/mascota/show.blade.php
+++ resources/views/mascota/show.blade.php
@@ -8,62 +8,49 @@
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="float-left">
-                            <span class="card-title">{{ __('Detalles de la Mascota') }}</span>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('mascotas.index') }}"> {{ __('Volver') }}</a>
+                <div class="card card-widget widget-user">
+                    <div class="widget-user-header bg-primary">
+                        <h3 class="widget-user-username">{{ $mascota->nombre }}</h3>
+                        <h5 class="widget-user-desc">{{ $mascota->raza->nombre ?? 'No especificada' }}</h5>
+                    </div>
+                    <div class="widget-user-image">
+                        @if($mascota->avatar)
+                            <img class="img-circle elevation-2" src="{{ asset('public/' . $mascota->avatar) }}" alt="Avatar de {{ $mascota->nombre }}">
+                        @else
+                            <img class="img-circle elevation-2" src="{{ asset('public/default-avatar.png') }}" alt="Avatar por defecto">
+                        @endif
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">{{ __('Edad') }}</h5>
+                                    <span class="description-text">{{ $mascota->edad }}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">{{ __('Género') }}</h5>
+                                    <span class="description-text">{{ $mascota->genero }}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="description-block">
+                                    <h5 class="description-header">{{ __('Barrio') }}</h5>
+                                    <span class="description-text">{{ $mascota->barrio->nombre ?? 'No especificado' }}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
+                </div>
+                <div class="card">
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <strong>{{ __('Avatar:') }}</strong>
-                                    @if($mascota->avatar)
-                                        <div class="mt-2">
-                                            <img src="{{ asset('storage/' . $mascota->avatar) }}" alt="Avatar de {{ $mascota->nombre }}" class="img-thumbnail" style="max-width: 150px;">
-                                        </div>
-                                    @else
-                                        <p>{{ __('No hay imagen disponible') }}</p>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <strong>{{ __('Nombre:') }}</strong>
-                                    {{ $mascota->nombre }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Edad:') }}</strong>
-                                    {{ $mascota->edad }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Género:') }}</strong>
-                                    {{ $mascota->genero }}
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <strong>{{ __('Fecha de Nacimiento:') }}</strong>
                                     {{ $mascota->fecha_nacimiento ? \Carbon\Carbon::parse($mascota->fecha_nacimiento)->format('d/m/Y') : 'No especificada' }}
                                 </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Raza:') }}</strong>
-                                    {{ $mascota->raza->nombre ?? 'No especificada' }} ({{ $mascota->raza->tipo_mascota ?? 'No especificada' }})
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Barrio:') }}</strong>
-                                    {{ $mascota->barrio->nombre ?? 'No especificado' }} ({{ $mascota->barrio->localidad ?? 'No especificada' }})
-                                </div>
-                            </div>
-                            <div class="col-md-6">
                                 <div class="form-group">
                                     <strong>{{ __('Vacunas Completas:') }}</strong>
                                     @if($mascota->vacunas_completas)
@@ -76,6 +63,8 @@
                                     <strong>{{ __('Última Vacunación:') }}</strong>
                                     {{ $mascota->ultima_vacunacion ? \Carbon\Carbon::parse($mascota->ultima_vacunacion)->format('d/m/Y') : 'No especificada' }}
                                 </div>
+                            </div>
+                            <div class="col-md-6">
                                 <div class="form-group">
                                     <strong>{{ __('Esterilizado:') }}</strong>
                                     @if($mascota->esterilizado)
@@ -84,34 +73,13 @@
                                         <span class="badge badge-danger">{{ __('No') }}</span>
                                     @endif
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-12">
                                 <div class="form-group">
                                     <strong>{{ __('Dirección:') }}</strong>
                                     {{ $mascota->direccion ?? 'No especificada' }}
                                 </div>
                                 <div class="form-group">
-                                    <strong>{{ __('Interior/Apartamento:') }}</strong>
-                                    {{ $mascota->interior_apto ?? 'No especificado' }}
-                                </div>
-                                <div class="form-group">
                                     <strong>{{ __('Comportamiento:') }}</strong>
                                     {{ $mascota->comportamiento ?? 'No especificado' }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Recomendaciones:') }}</strong>
-                                    {{ $mascota->recomendaciones ?? 'No especificadas' }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Enfermedades:') }}</strong>
-                                    {{ $mascota->enfermedades ?? 'No especificadas' }}
-                                </div>
-                                <div class="form-group">
-                                    <strong>{{ __('Último Examen Médico:') }}</strong>
-                                    {{ $mascota->ultimo_examen_medico ? \Carbon\Carbon::parse($mascota->ultimo_examen_medico)->format('d/m/Y') : 'No especificado' }}
                                 </div>
                             </div>
                         </div>
