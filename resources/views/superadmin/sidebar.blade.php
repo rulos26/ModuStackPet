f92a80a855130398ad701692298b4710a0e73df0
juan.diaz <rulos26@gmail.com>
2025-04-17 20:37:24 -0500

    Actualizar enlace del sidebar de Superadmin: se modifica la ruta del enlace del dashboard para dirigir a la ruta específica de superadmin, mejorando la consistencia en la navegación.

--- resources/views/superadmin/sidebar.blade.php
+++ resources/views/superadmin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('superadmin.dashboard') }}">
         <div class="sidebar-brand-icon">
             <i class="fas fa-paw"></i>
         </div>
