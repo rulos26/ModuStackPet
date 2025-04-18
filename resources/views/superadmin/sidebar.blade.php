9a8af4012e913fca250050c71ede330142c74e24
juan.diaz <rulos26@gmail.com>
2025-04-17 20:38:02 -0500

    Eliminar enlace del dashboard en el sidebar de Superadmin para simplificar la navegación y mejorar la claridad del código.

--- resources/views/superadmin/sidebar.blade.php
+++ resources/views/superadmin/sidebar.blade.php
@@ -12,13 +12,7 @@
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('dashboard') }}">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
-        </a>
-    </li>
+   
 
     <!-- Divider -->
     <hr class="sidebar-divider">
